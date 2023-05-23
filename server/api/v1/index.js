d3808ad70ba59dabc9efeac6aa61242e784059df
Elton John Acevedo Yazon <eltonjohnyazon@gmail.com>
2023-05-23 18:49:10 +0800

    + added swagger ui docs

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,11 +1,21 @@
+require('../../globals');
 const express = require('express');
 const router = new express.Router();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerApiV1Docs = YAML.load(
+  path.join(__serverRoot, 'documentation/apiv1.yaml')
+);
 // const tasks = require('./tasks');
 const users = require('./users');
 
 module.exports = () => {
   // router.use('/tasks', tasks());
   router.use('/users', users);
+  router.use('/docs', swaggerUi.serve, (req, res) => {
+    swaggerUi.setup(swaggerApiV1Docs)(req, res);
+  });
 
   router.use('*', (req, res) => {
     res.status(404).json({
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(morgan('dev'));
+app.use(bodyParser.json())
 
 require('./globals');
 
